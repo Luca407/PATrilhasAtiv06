8639624189f555ffed440f870ad709ae063e323b
Luca407 <0001081825@senaimgaluno.com.br>
2025-02-06 10:17:28 -0300

    ZeroACem.cs

---
+++ ZeroACem.cs
@@ -0,0 +1,16 @@
+using System;
+
+ class Program
+    {
+        public static void Main(string[] args)
+        {
+        
+        	for (int i = 0; i <=100; i++)
+        	{
+        		Console.WriteLine(i);
+        	}
+       
+            Console.ReadKey();
+        }
+        
+    }
