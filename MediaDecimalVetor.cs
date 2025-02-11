586421317bc52af12e89bd35253a9312ec70e14f
Luca407 <0001081825@senaimgaluno.com.br>
2025-02-10 22:31:07 -0300

    MediaDecimalVetor.cs

---
+++ MediaDecimalVetor.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double[] numeros = { 5.5, 7.2, 8.3, 6.4, 9.1 };
+        double soma = 0;
+
+        for (int i = 0; i < numeros.Length; i++)
+        {
+            soma += numeros[i];
+        }
+
+        double media = soma / numeros.Length;
+        Console.WriteLine("Média dos valores: " + media);
+    }
+}
